8442f79e1d0d79da637199d0e5f4da2ccf54651c
Wend Carlos <wendcarlos18@hotmail.com>
2022-01-30 17:24:25 -0300

    Installed and configured eslint-plugin-import-helpers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
 import Login from './containers/Login'
 
 ReactDOM.render(
   <Login/>,
   document.getElementById('root')
-);
-
-
+)
