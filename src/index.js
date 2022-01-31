e3a32b074178c4efa4fe3567afe6887ea99d21c5
Wend Carlos <wendcarlos18@hotmail.com>
2022-01-31 11:26:46 -0300

    I installed Styled Components. I created the styles/globalStyles.js folder. I made the global styles of the application.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Login from './containers/Login'
+import GlobalStyles from './styles/globalStyles'
 
-ReactDOM.render(<Login />, document.getElementById('root'))
+ReactDOM.render(
+    <>
+        <Login />
+        <GlobalStyles />
+    </>,
+    document.getElementById('root')
+)
