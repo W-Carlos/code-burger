fab56403b6d81898f78f3b2134226cfe38e3657f
Wend Carlos <wendcarlos18@hotmail.com>
2022-02-16 16:25:50 -0300

    adding the context to display the information

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -7,6 +7,7 @@ import * as Yup from 'yup'
 import LoginImg from '../../assets/login-image.svg'
 import Logo from '../../assets/logo.svg'
 import Button from '../../components/Button'
+import { useUser } from '../../hooks/UserContext'
 import api from '../../services/api'
 import {
     Container,
@@ -19,6 +20,10 @@ import {
 } from './styles'
 
 function Login() {
+    const users = useUser()
+
+    console.log(users)
+
     /* Validação do formulario */
     const schema = Yup.object().shape({
         email: Yup.string()
--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ToastContainer } from 'react-toastify'
 
-/* import Login from './containers/Login' */
-import Register from './containers/Register'
+import Login from './containers/Login'
+/* import Register from './containers/Register' */
+import { UserProvider } from './hooks/UserContext'
 import GlobalStyles from './styles/globalStyles'
 
 ReactDOM.render(
     <>
-        <Register />
+        <UserProvider>
+            <Login />
+        </UserProvider>
         <ToastContainer autoClose={2000} theme="colored" />
         <GlobalStyles />
     </>,
