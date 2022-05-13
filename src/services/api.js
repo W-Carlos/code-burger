eb4fd83b943495d4e998219d2a500475110d9d92
Wend Carlos <wendcarlos18@hotmail.com>
2022-05-13 20:14:25 -0300

    I added the JWT Token to the API calls

--- src/services/api.js
+++ src/services/api.js
@@ -5,4 +5,14 @@ const apiCodeBurger = axios.create({
     baseURL: 'http://localhost:3001'
 })
 
+// Interceptando um arequisição. Antes de enviar a requisição vai ser Adicionado Token JWT nas chamadas da API
+apiCodeBurger.interceptors.request.use(async config => {
+    // Pegando o token no localStorage
+    const userData = await localStorage.getItem('codeburger: userData')
+    const token = userData && JSON.parse(userData).token
+    // Colocando o token dentro da config
+    config.headers.authorization = `Bearer ${token}`
+    return config
+})
+
 export default apiCodeBurger
