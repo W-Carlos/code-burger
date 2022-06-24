9dab468fa57385ccf8d824306c1cd6011aca530f
Wend Carlos <wendcarlos18@hotmail.com>
2022-06-23 21:27:08 -0300

    Changing Hooks import

---
+++ src/hooks/index.js
@@ -0,0 +1,14 @@
+/* Este arquivo tem todos os providers */
+
+import PropTypes from 'prop-types'
+import React from 'react'
+
+import { UserProvider } from './UserContext'
+
+const AppProvider = ({ children }) => <UserProvider>{children}</UserProvider>
+
+AppProvider.propTypes = {
+    children: PropTypes.node
+}
+
+export default AppProvider
