170d1d800ba791c79fc2cda59522674ce67e97a6
Wend Carlos <wendcarlos18@hotmail.com>
2022-07-06 12:06:31 -0300

    Sending request to API

--- src/components/CartResume/index.js
+++ src/components/CartResume/index.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
+import { toast } from 'react-toastify'
 
 import { useCart } from '../../hooks/CartContext'
+import api from '../../services/api'
 import formatCurrency from '../../utils/formatCurrency'
 import { Button } from '../Button'
 import { Container } from './styles'
@@ -21,6 +23,19 @@ export function CartResume() {
         setFinalPrice(sumAllItems)
     }, [cartProducts, deliveryTax])
 
+    // Enviando pedidos para a API
+    const submitOrder = async () => {
+        const order = cartProducts.map(product => {
+            return { id: product.id, quantity: product.quantity }
+        })
+
+        await toast.promise(api.post('orders', { products: order }), {
+            pending: 'Realizando seu pedido...',
+            success: 'Pedido realizado com sucesso!',
+            error: 'Falha ao tentar realizar o seu pedido, tente novamente!'
+        })
+    }
+
     return (
         <div>
             <Container>
@@ -38,7 +53,10 @@ export function CartResume() {
                     <p>{formatCurrency(finalPrice + deliveryTax)}</p>
                 </div>
             </Container>
-            <Button style={{ width: '100%', marginTop: 30 }}>
+            <Button
+                style={{ width: '100%', marginTop: 30 }}
+                onClick={submitOrder}
+            >
                 Finalizar pedido
             </Button>
         </div>
