78bebe6999f1c8b5cc33f29d8b94a453cbfb6270
Wend Carlos <wendcarlos18@hotmail.com>
2022-07-26 11:39:10 -0300

    Creating status selection menu. I made the menu filter logic

--- src/containers/Admin/Orders/order-status.js
+++ src/containers/Admin/Orders/order-status.js
@@ -3,26 +3,31 @@
 const status = [
     {
         id: 1,
+        label: 'Todos',
+        value: 'Todos'
+    },
+    {
+        id: 2,
         label: 'Pedido realizado',
         value: 'Pedido realizado'
     },
     {
-        id: 2,
+        id: 3,
         label: 'Em preparação',
         value: 'Em preparação'
     },
     {
-        id: 3,
+        id: 4,
         label: 'Pedido pronto',
         value: 'Pedido pronto'
     },
     {
-        id: 4,
+        id: 5,
         label: 'Pedido a caminho',
         value: 'Pedido a caminho'
     },
     {
-        id: 5,
+        id: 6,
         label: 'Entregue',
         value: 'Entregue'
     }
