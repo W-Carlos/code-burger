9dad0727687a0be32c1eebe57010d70bfb868085
Wend Carlos <wendcarlos18@hotmail.com>
2022-07-30 21:06:52 -0300

    Creating side menu. I made the structure and style of the menu.

---
+++ src/components/SideMenuAdmin/menu-list.js
@@ -0,0 +1,13 @@
+/* Informações dos links do menu */
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag'
+
+const listLinks = [
+    {
+        id: 1,
+        label: 'Pedidos',
+        link: '/pedidos',
+        icon: ShoppingBagIcon
+    }
+]
+
+export default listLinks
--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import { SideMenuAdmin } from '../../components/SideMenuAdmin'
 import Orders from './Orders'
 import { Container } from './styles'
 
 export function Admin() {
     return (
         <Container>
+            <SideMenuAdmin />
             <Orders />
         </Container>
     )
